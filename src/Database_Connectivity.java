8a2f3a0d8acfa1b4edf9435f5e88412fc97fe9cd
mraab89 <mario.raab89@web.de>
2014-05-29 00:33:42 +0200

    try/catch procedure and initialize driver

--- src/Database_Connectivity.java
+++ src/Database_Connectivity.java
@@ -1,4 +1,22 @@
 
+import java.sql.*; //import SQL-functions 
+
 public class Database_Connectivity {
 
+	public static void main(String[] args){
+		
+		try{
+			
+			//initialize Driver
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			
+			
+		}
+		
+		catch(){
+			
+			//TODO failure message
+		}
+	}
+	
 }
